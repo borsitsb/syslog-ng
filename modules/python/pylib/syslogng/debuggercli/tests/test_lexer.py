c4aa4ab238fb6b3f8ccb4dfbf67eb83c9ee2cc84
Balazs Scheidler <balazs.scheidler@balabit.com>
2015-03-05 17:14:48 +0100

    debuggercli: add Lexer base class

---
+++ modules/python/pylib/syslogng/debuggercli/tests/test_lexer.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class TestLexer(unittest.TestCase):
+
+    def setUp(self):
+        self._lexer = self._construct_lexer()
+        self._current_token = None
+
+    def _construct_lexer(self):
+        raise NotImplementedError
+
+    def _next_token(self):
+        self._current_token = self._lexer.token()
+        return self._current_token
+
+    def _assert_current_token_type_equals(self, token_type):
+        self.assertEquals(self._current_token.type, token_type)
+
+    def _assert_current_token_value_equals(self, value):
+        self.assertEquals(self._current_token.value, value)
+
+    def _assert_current_token_is_partial(self):
+        self.assertTrue(self._current_token.partial)
+
+    def _assert_current_token_is_not_partial(self):
+        self.assertFalse(self._current_token.partial)
+
+    def _assert_next_token_type_equals(self, token_type):
+        self._next_token()
+        self._assert_current_token_type_equals(token_type)
+
+    def _assert_next_token_is_partial(self):
+        self._next_token()
+        self._assert_current_token_is_partial()
+
+    def _assert_next_token_is_none(self):
+        self.assertIsNone(self._next_token())
+
+    def _assert_next_token_value_equals(self, value):
+        self._next_token()
+        self._assert_current_token_value_equals(value)
